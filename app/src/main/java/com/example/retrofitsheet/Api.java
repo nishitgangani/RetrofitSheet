aa44d4b29bd972f6a650017253dbf4daa0a2ce85
nishitgangani <nishitgangani29@gmail.com>
2021-02-17 11:37:30 +0530

    RetrofitSheet

--- app/src/main/java/com/example/retrofitsheet/Api.java
+++ app/src/main/java/com/example/retrofitsheet/Api.java
@@ -10,6 +10,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface Api {
+//    https://script.google.com/macros/s/AKfycbxOLElujQcy1-ZUer1KgEvK16gkTLUqYftApjNCM_IRTL3HSuDk/exec?id=159--pHYbfd56xjdLpNau0Db58Qz8L2r8kGf-WY8l8Ww&sheet=Sheet1
     String BASE_URL = "https://script.google.com/macros/s/AKfycbxOLElujQcy1-ZUer1KgEvK16gkTLUqYftApjNCM_IRTL3HSuDk/";
     @GET("exec")
     Call<Model> getmodel(@Query("id") String id, @Query("sheet") String sheet);
